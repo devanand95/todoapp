c6d9f88940a1a73884b3b9393bc95b856e8f5c10
devanand <ndevanand95@gmail.com>
2021-05-22 03:50:18 -0400

    adding destroy and refactoring - end of lec 38

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+
+	before_action :set_todo, only: [:edit, :update, :show, :destroy]
 	def new
 		@todo = Todo.new
 	end
@@ -14,15 +16,12 @@ class TodosController < ApplicationController
 	end
 
 	def show
-		@todo = Todo.find(params[:id])
 	end
 
 	def edit
-		@todo = Todo.find(params[:id])
 	end
 
 	def update
-		@todo = Todo.find(params[:id])
 		if @todo.update(todo_params)
 			flash[:notice] = "Todo was successfully updated"
 		  redirect_to todo_path(@todo)
@@ -35,9 +34,19 @@ class TodosController < ApplicationController
 		@todos = Todo.all
 	end
 
+	def destroy
+		@todo.destroy
+		flash[:notice] = "Todo was successfully deleted"
+		redirect_to todos_path
+	end
+
 	private
 		def todo_params
 			params.require(:todo).permit(:name,:description)
+		end
+
+		def set_todo
+			@todo = Todo.find(params[:id])
 		end
 
 end
