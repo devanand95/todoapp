7304b30d9dfea24ad87fd41e7cd667919c3e6114
devanand <ndevanand95@gmail.com>
2021-05-22 03:06:55 -0400

    Edit action todo - lec 34 end

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,10 +6,29 @@ class TodosController < ApplicationController
 	def create
 		@todo = Todo.new(todo_params)
 		if @todo.save
+			flash[:notice] = "Todo was created successfully"
 		  redirect_to todo_path(@todo)
 	    else
 	      render 'new'
 	    end 
+	end
+
+	def show
+		@todo = Todo.find(params[:id])
+	end
+
+	def edit
+		@todo = Todo.find(params[:id])
+	end
+
+	def update
+		@todo = Todo.find(params[:id])
+		if @todo.update(todo_params)
+			flash[:notice] = "Todo was successfully updated"
+		  redirect_to todo_path(@todo)
+	    else
+	      render 'edit'
+	    end
 	end
 
 	private
